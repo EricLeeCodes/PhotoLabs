570decdffe51c5a4ca1994ba74e00de1066d6a70
Eric Lee <ericleecodes@gmail.com>
2024-03-20 20:19:21 -0700

    Add CSS from PhotoListItem.scss to PhotoListItem.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,17 +1,28 @@
-import React from "react";
-
+import React, { Fragment } from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <>
-      <img key={props.photo.id} src={props.photo.imageSource}></img>
-      <img src={props.photo.profile}></img>
-      <div>{props.photo.username}</div>
-      <div>
-        <div>{`${props.photo.location.city} ${props.photo.location.country}`}</div>
+    <div className="photo-list__item">
+      <img
+        className="photo-list__image"
+        key={props.photo.id}
+        src={props.photo.imageSource}
+      ></img>
+      <div className="photo-list__user-details"></div>
+      <div className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={props.photo.profile}
+        ></img>
+        <div className="photo-list__user-info">
+          <div>{props.photo.username}</div>
+          <div className="photo-list__user-location">
+            {`${props.photo.location.city} ${props.photo.location.country}`}
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
