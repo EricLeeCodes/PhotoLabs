3b023840c72dcc75197bcf175d2ffabe28955609
Eric Lee <ericleecodes@gmail.com>
2024-03-21 14:17:08 -0700

    Create list container for photo items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,30 @@
 import React from "react";
-
-import PhotoListItem from "./components/PhotoListItem";
 import "./App.scss";
 import PhotoFavButton from "components/PhotoFavButton";
+import PhotoListItem from "./components/PhotoListItem";
+import PhotoList from "components/PhotoList";
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const displayPhotos = (input) => {
-  const photos = [];
-  for (let i = 0; i < input; i++) {
-    photos.push(<PhotoListItem key={i} photo={sampleDataForPhotoListItem} />);
-  }
-
-  return photos;
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  return <div className="App">{displayPhotos(3)}</div>;
+  return (
+    <div className="App">
+      {/* {Array.from(Array(3)).map((_, index) => (
+      <PhotoListItem key={index} photo={sampleDataForPhotoListItem} />
+
+      ))} */}
+    </div>
+  );
 };
 
 export default App;
