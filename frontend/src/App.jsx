1455ddee960d661ba51ab1a406657a9ce0a1cccd
Eric Lee <ericleecodes@gmail.com>
2024-03-21 18:25:17 -0700

    Populate photo and topic-navbar components with mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,27 +2,10 @@ import React from "react";
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
 
-// const sampleDataForPhotoListItem = {
-//   id: "1",
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// };
-
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {/* {Array.from(Array(3)).map((_, index) => (
-      <PhotoListItem key={index} photo={sampleDataForPhotoListItem} />
-
-      ))} */}
-      {/* <PhotoList /> */}
-      {/* <TopicList /> */}
       <HomeRoute />
     </div>
   );
