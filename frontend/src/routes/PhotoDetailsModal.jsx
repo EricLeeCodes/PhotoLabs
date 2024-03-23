915ed00f733ffcd6e972f31d5ea1f7d6ff611d95
Eric Lee <ericleecodes@gmail.com>
2024-03-22 23:12:32 -0700

    Layout modal component

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
+import PhotoFavButton from "components/PhotoFavButton";
+import PhotoList from "components/PhotoList";
 
 const PhotoDetailsModal = (props) => {
   return (
@@ -13,7 +15,32 @@ const PhotoDetailsModal = (props) => {
         <img src={closeSymbol} alt="close symbol" />
       </button>
 
-      <img className="photo-details-modal__image" src={props.photo.urls.full} />
+      <div className="move-left">
+        <PhotoFavButton />
+        <img
+          className="photo-details-modal__image"
+          src={props.photo.urls.full}
+        />
+      </div>
+
+      <div className="photo-details-modal__top-bar">
+        <div className="photo-details-modal__photographer-details">
+          <img
+            className="photo-details-modal__photographer-profile"
+            src={props.photo.user.profile}
+          />
+          <div className="photo-details-modal__header photo-details-modal__photographer-info">
+            <div>{props.photo.user.name}</div>
+            <div className="photo-details-modal__photographer-location">
+              {`${props.photo.location.city}, ${props.photo.location.country}`}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="photo-details-modal__images">
+        <PhotoList />
+      </div>
     </div>
   );
 };
