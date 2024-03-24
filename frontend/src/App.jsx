5f855285d37514f0b37d7c59ce09efc072377c03
Eric Lee <ericleecodes@gmail.com>
2024-03-23 21:14:30 -0700

    Organize state data and functions using custom hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
 import photos from "mocks/photos";
 import topics from "mocks/topics";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
-import useApplicationData from "./hooks/useApplicationData";
+import useApplicationData from "hooks/useApplicationData.js";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const { displayModal, selectedPhoto, favorites } = useApplicationData();
-
-  const displayModalHandler = (isDisplaying, photo) => {
-    setDisplayModal(isDisplaying);
-    return setSelectedPhoto(photo);
-  };
-
-  const toggleFavorite = (id) => {
-    setFavorites((favorites) => {
-      if (favorites.includes(id)) {
-        return favorites.filter((favId) => favId !== id);
-      } else {
-        return [...favorites, id];
-      }
-    });
-  };
+  const {
+    displayModal,
+    selectedPhoto,
+    favorites,
+    displayModalHandler,
+    toggleFavorite,
+  } = useApplicationData();
 
   return (
     <div className="App">
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,10 +1,29 @@
-import { useState } from "module";
+import { useState } from "react";
+
 
 const useApplicationData = () => {
   const [displayModal, setDisplayModal] = useState(false);
   const [selectedPhoto, setSelectedPhoto] = useState({});
   const [favorites, setFavorites] = useState([]);
+
+  const displayModalHandler = (isDisplaying, photo) => {
+    setDisplayModal(isDisplaying);
+    return setSelectedPhoto(photo);
+  };
+
+  const toggleFavorite = (id) => {
+    setFavorites((favorites) => {
+      if (favorites.includes(id)) {
+        return favorites.filter((favId) => favId !== id);
+      } else {
+        return [...favorites, id];
+      }
+    });
+  };
+
+  return { displayModal, selectedPhoto, favorites, displayModalHandler, toggleFavorite };
+
 };
 
 
-export default { useApplicationData };
+export default useApplicationData;
